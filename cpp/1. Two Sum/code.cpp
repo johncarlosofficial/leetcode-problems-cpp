74a658f1348ee0d9c1e9de67453ccab38c9f70ae
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-02-21 18:50:34 -0300

    Two Sum

---
+++ cpp/1. Two Sum/code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int> answer;
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if((nums[i]+nums[j])==target){
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    break;
+                }
+            }
+        }
+    return answer;
+    }
+};
